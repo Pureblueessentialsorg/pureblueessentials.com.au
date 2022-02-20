35761b6d617689af31392113bfe2b7330b69eee3
sean-pwd <sean@powerfulwebdesign.com.au>
2022-02-19 19:57:29 -0500

    fix: updated js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,13 +1,13 @@
-import contactForm  from '@future-wd/contact-form-xhr';
-!process.env.HUGO_ENVIRONMENT === 'development'
-? prod()
-: dev;
+// import contactForm  from '@future-wd/contact-form-xhr';
+import contactForm from 'js/contact-form-xhr'
+import * as params from '@params';
+
 const dev = contactForm('js-contactForm',{
   formAction: 'https://mail.powerfulwebdesign.com.au/v2/index.php', // online
   // formAction: 'http://localhost/mail.php', // local test
   account: 'local',
   grecaptchaKey: '6LdUwwweAAAAAA9iDDa0A5RosBBoagSnU1yBvUQ3', // local test
-  debug: true,
+  //debug: true,
 });
 
 const prod = contactForm('js-contactForm',{
@@ -15,3 +15,7 @@ const prod = contactForm('js-contactForm',{
   account: 'pbe',
   grecaptchaKey: '6LfyBEoeAAAAAGgMEs1aU_V-NRaJsKozabroPWoN',
 });
+
+params.env === 'development'
+? dev
+: prod;
