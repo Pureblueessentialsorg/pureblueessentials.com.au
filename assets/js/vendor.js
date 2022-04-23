63db9528f9c66b4b5b813d5d7b6cc287a0f91e72
sean-pwd <sean@powerfulwebdesign.com.au>
2022-04-22 22:14:48 -0400

    fix: update search

--- assets/js/vendor.js
+++ assets/js/vendor.js
@@ -67,44 +67,46 @@ var firstRun = true; // allow us to delay loading json data unless search activa
 
 const search = document.getElementById('js-search');
 const searchInput = document.getElementById('js-searchInput');
-const searchResults =  document.getElementById('js-searchResults');
+const searchResults = document.getElementById('js-searchResults');
+const loading = document.getElementById('js-searchLoading');
 const hiddenClass = 'd-none';
 
-
-
-searchInput.addEventListener('focus', function() {
-      // Load json search index if first time invoking search
+searchInput.addEventListener('focus', function () {
+  // Load json search index if first time invoking search
   // Means we don't load json unless searches are going to happen; keep user payload small unless needed
-  if(firstRun) {
+  if (firstRun) {
     loadSearch(); // loads our json data and builds fuse.js search index
     firstRun = false; // let's never do this again
   }
 }, false);
 
-// search.addEventListener('submit', function(event) {
-//   event.preventDefault();
-//   executeSearch(searchInput.value);
-// });
+// disable enter when cursor inside the search box
+search.addEventListener('submit', function (event) {
+  event.preventDefault();
+  executeSearch(searchInput.value);
+});
 
 
 // ==========================================
 // execute search as each character is typed
 // keyup used to that blank text can be detected on backspace
-searchInput.addEventListener('keyup', function(event) { 
+searchInput.addEventListener('keyup', function (event) {
   const text = this.value
   if ((event.key === 'Backspace') && (text === '')) {
     searchResults.classList.add(hiddenClass);
   } else {
+    searchResults.classList.remove(hiddenClass);
     executeSearch(text);
+
   }
 });
 
 // close results on focus out
-document.addEventListener('click', function(event) {
-    var isClickInsideElement = searchInput.contains(event.target);
-    if (!isClickInsideElement) {
-      searchResults.classList.add(hiddenClass);
-    }
+document.addEventListener('click', function (event) {
+  var isClickInsideElement = searchInput.contains(event.target);
+  if (!isClickInsideElement) {
+    searchResults.classList.add(hiddenClass);
+  }
 });
 
 
@@ -113,16 +115,16 @@ document.addEventListener('click', function(event) {
 //
 function fetchJSONFile(path, callback) {
   var httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function() {
+  httpRequest.onreadystatechange = function () {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200) {
         var data = JSON.parse(httpRequest.responseText);
-          if (callback) callback(data);
+        if (callback) callback(data);
       }
     }
   };
   httpRequest.open('GET', path);
-  httpRequest.send();  
+  httpRequest.send();
 }
 
 
@@ -130,25 +132,27 @@ function fetchJSONFile(path, callback) {
 // load our search index, only executed once
 // on focus
 //
-function loadSearch() { 
-  fetchJSONFile('/index.json', function(data){
+function loadSearch() {
+  fetchJSONFile('/index.json', function (data) {
 
     var options = { // fuse.js options; check fuse.js website for details
       shouldSort: true,
-      location: 0,
-      distance: 100,
-      threshold: 0.4,
+      // location: 0,
+      // distance: 100,
+      threshold: 0.3, // 0.0 requires perfect score, 1 matches anything. default 0.6
       minMatchCharLength: 2,
+      findAllMatches: true, // finishes searching pattern, even if an exact match is found
+      ignoreLocation: true, // match anywhere in the strings
       keys: [
         'title',
-        'permalink',
+        // 'permalink',
         // 'contents',
-        ],
+        'categories',
+      ],
     };
     fuse = new Fuse(data, options); // build the index from the json file
   });
 }
-
 
 // ==========================================
 // using the index we loaded
@@ -157,26 +161,30 @@ function loadSearch() {
 //
 function executeSearch(term) {
   let results = fuse.search(term); // the actual query being run using fuse.js
-  let searchitems = ''; // our results bucket
-  searchResults.classList.remove(hiddenClass);
   if (results.length === 0) { // no results based on what was typed into the input box
-    //resultsAvailable = false;
-    // searchitems = '';
-    searchitems = searchitems + '<li><div class="title">No items found</div></li>';
-  } else { // build our html 
-
-    for (let item in results.slice(0,5)) { // only show first 5 results
-      searchitems = searchitems + 
-        '<li><a href="' + results[item].item.permalink + '" tabindex="0">' + 
-        '<div class="title">' + results[item].item.title + '</div>';
-        // '<br><span class="sc">'+ results[item].section +'</span> — ' + results[item].date + ' — <em>' + results[item].desc + '</em></a></li>';
-    }
-    //resultsAvailable = true;
+    searchResults.innerHTML = '<div class="results__empty">No items found</div>';
+  } else {
+    // hide loading
+    loading.classList.add(hiddenClass);
+    // clear div
+    searchResults.innerHTML = '';
+    // add each result to the div
+    results.forEach((value, key) => {
+      const { title, permalink, image, categories } = value.item;
+      const output = `
+        <div class="results__container" id="result-${key}">
+          <div class="results__row">
+            <div class="results__col--4">
+              <img src="${image}" class="results__image" /> 
+            </div>
+            <div class="results__col--8"> 
+              <div class="results__title"><a href="${permalink}">${title}</a></div>
+              <div class="text-muted">${categories.join(', ')}</div>
+            </div>
+          </div>
+        </div>`;
+      // add result to div
+      searchResults.innerHTML += output;
+    })
   }
-
-  searchResults.innerHTML = searchitems;
-  // if (results.length > 0) {
-  //   first = list.firstChild.firstElementChild; // first result container — used for checking against keyboard up/down location
-  //   last = list.lastChild.firstElementChild; // last result container — used for checking against keyboard up/down location
-  // }
 }
