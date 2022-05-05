693c63fc5ffb6e79aad106820ab375b993c97aa8
sean-pwd <sean@powerfulwebdesign.com.au>
2022-05-04 23:54:21 -0400

    disable scroll to top for xs

--- assets/js/assets.js
+++ assets/js/assets.js
@@ -1,36 +1,41 @@
 document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/,'js');
 
 
+// import { Tooltip } from 'bootstrap';
 // import core-js so babel can provide necessarty polyfills
 // import 'core-js/stable';
 // apply year to footer
 // document.getElementById('year').innerHTML = new Date().getFullYear();
 
-const distance = 300;
-const parentId = 'js-topNav';
-const hiddenClass = 'd-none';
-
-document.addEventListener("DOMContentLoaded", function (event) {
+import {topNav} from './top-nav.js';
+topNav();
 
 
-  const badge = document.getElementById(parentId);  //id of nav bar parent div
-  const show = function (elem) {
-    elem.classList.remove(hiddenClass);
-  };
-  const hide = function (elem) {
-    elem.classList.add(hiddenClass);
-  };
+// const distance = 300;
+// const parentId = 'js-topNav';
+// const hiddenClass = 'd-none';
 
-  const showOnScroll = function () {
-    if (document.body.scrollTop > distance || document.documentElement.scrollTop > distance) {
-      show(badge);
-    } else {
-      hide(badge);
-    }
-  }
-  if (badge) {
-    window.addEventListener('scroll', showOnScroll);
-  }
+// document.addEventListener("DOMContentLoaded", function (event) {
 
 
-});
+//   const badge = document.getElementById(parentId);  //id of nav bar parent div
+//   const show = function (elem) {
+//     elem.classList.remove(hiddenClass);
+//   };
+//   const hide = function (elem) {
+//     elem.classList.add(hiddenClass);
+//   };
+
+//   const showOnScroll = function () {
+//     if (document.body.scrollTop > distance || document.documentElement.scrollTop > distance) {
+//       show(badge);
+//     } else {
+//       hide(badge);
+//     }
+//   }
+//   if (badge) {
+//     window.addEventListener('scroll', showOnScroll);
+//   }
+
+
+// });
--- assets/js/top-nav.js
+++ assets/js/top-nav.js
@@ -3,21 +3,26 @@ export function topNav({  //defaults
   distance = 300,
   parentId = 'js-topNav',
   hiddenClass = 'd-none',
+  breakPoint = 576, // sm and larger
 } = {}) {
 
-window.addEventListener('scroll', showOnScroll);
-const badge = document.getElementById(parentId);  //id of nav bar parent div
-const show = function (elem) {
-  elem.classList.remove(hiddenClass);
-};
-const hide = function (elem) {
-  elem.classList.add(hiddenClass);
-};
-function showOnScroll() {
-  if (document.body.scrollTop > distance || document.documentElement.scrollTop > distance) {
-    show(badge);
-  } else {
-    hide(badge);
+  if (window.innerWidth >= breakPoint) {
+    console.log(`width is greater than ${breakPoint}, with is ${window.innerWidth}`)
+    window.addEventListener('scroll', showOnScroll);
+    const badge = document.getElementById(parentId);  //id of nav bar parent div
+    const show = function (elem) {
+      elem.classList.remove(hiddenClass);
+    };
+    const hide = function (elem) {
+      elem.classList.add(hiddenClass);
+    };
+    function showOnScroll() {
+      if (document.body.scrollTop > distance || document.documentElement.scrollTop > distance) {
+        show(badge);
+      } else {
+        hide(badge);
+      }
+    }
   }
-}
+ 
 }
