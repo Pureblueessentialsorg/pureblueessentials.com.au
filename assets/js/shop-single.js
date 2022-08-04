45f3d457fe0d5704b6534e505bc151ad1a7fd69b
Sean Emerson <emerson.sean.dev@gmail.com>
2022-08-03 23:35:07 -0800

    feat: auto expire date on price json-ld

--- assets/js/shop-single.js
+++ assets/js/shop-single.js
@@ -19,6 +19,22 @@ update('js-long', 4);
 update('js-designs', 3);
 update('js-fillings', 4);
 
+// update json-ld with a price expiry date, 1 year from today
+// get element
+const jsonOrig = document.getElementById('json-product')
+if (jsonOrig) {
+  // get todays date, add 1 year and format as YYY-MM-DD
+  const today = new Date()
+  const oneYearFull = today.setFullYear(today.getFullYear() + 1)
+  const oneYearShort = new Date(oneYearFull).toISOString().slice(0, 10);
+  // parse original json-ld
+  const jsonParsed = JSON.parse(jsonOrig.innerText)
+  // add valid until
+  jsonParsed.offers.priceValidUntil = oneYearShort
+  // strinfigy and replace original
+  jsonOrig.innerText = JSON.stringify(jsonParsed)
+}
+
 
 
 
