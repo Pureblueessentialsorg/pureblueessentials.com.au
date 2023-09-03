4b2a0fe31bc33a1b8d5a1bc0c3ffafdbf1837cbb
Sean Emerson <emerson.sean.dev@gmail.com>
2023-09-03 21:58:13 +1000

    update endpoint

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,14 +2,14 @@ import contactForm from 'js/contact-form-xhr'
 import * as params from '@params';
 
 // production config
-let formAction = 'https://mail2.powerfulwebdesign.com.au/index.php';
+let formAction = 'https://api.rapidforms.com.au/index.php';
 let account = 'pbe';
 let grecaptchaKey = '6LfyBEoeAAAAAGgMEs1aU_V-NRaJsKozabroPWoN';
 let debug = false;
 
 //dev config
 if (params.env === 'development') {
-  formAction = 'https://mail2.powerfulwebdesign.com.au/index.php'; // online
+  formAction = 'https://api.rapidforms.com.au/index.php'; // online
   // formAction = 'http://localhost/index.php'; // local test
   account = 'local';
   grecaptchaKey = '6LdUwwweAAAAAA9iDDa0A5RosBBoagSnU1yBvUQ3'; // local test
